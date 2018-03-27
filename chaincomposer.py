6c92371453c7c9e657405b6482b2cc088bbe6f49
Ryan Freckleton <ryan.freckleton@gmail.com>
2018-03-27 15:29:40 -0600

    Implement second test

--- chaincomposer.py
+++ chaincomposer.py
@@ -6,6 +6,11 @@ class ChainComposer:
     def __init__(self, view):
         self.view = view
         self.filters = (ReverbFilter,)
+        self.chain = ()
 
     def initialize(self):
         self.view.set_selection(self.filters)
+
+    def add(self, index):
+        self.chain = self.chain + (self.filters[index],)
+        self.view.set_chain(self.chain)
--- test_chaincomposer.py
+++ test_chaincomposer.py
@@ -4,9 +4,13 @@ from chaincomposer import ChainComposer
 class MockChainComposerView:
     def __init__(self):
         self.selection = ()
+        self.chain = ()
 
     def set_selection(self, selection):
         self.selection = selection
+
+    def set_chain(self, chain):
+        self.chain = chain
 
 
 class TestChainComposer:
@@ -16,3 +20,12 @@ class TestChainComposer:
         composer = ChainComposer(view)
         composer.initialize()
         assert len(view.selection) == 1
+
+    def test_select_filter(self):
+        view = MockChainComposerView()
+        composer = ChainComposer(view)
+        composer.initialize()
+
+        composer.add(0)
+
+        assert len(view.chain) == 1
